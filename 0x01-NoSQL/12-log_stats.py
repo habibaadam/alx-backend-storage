db6383102c33dc9f3bdae2e4b4da89e3a88c5fc1
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-26 21:01:11 +0000

    Adding comments to task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,11 +7,15 @@ import pymongo
 
 
 if __name__ == '__main__':
+    # First try to  connect to the mongo database with pymongo
     server = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+
+    # Get a count of all the documents in the collection
     x = server.logs.nginx
     docs = x.count_documents({})
     print(f"{docs} logs")
 
+    # Print the http methods alongside a count of their documents
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
 
@@ -19,5 +23,6 @@ if __name__ == '__main__':
         num_of_docs = x.count_documents({"method": methods})
         print(f"\t method {method}: {num_of_docs}")
 
+    # Count and print out the number of get requests with status check
     status_count = x.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_count} status check")
