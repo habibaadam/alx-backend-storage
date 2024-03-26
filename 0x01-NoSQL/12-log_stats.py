36c57216860d15cc1c06e5c220693d08895efb84
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-26 20:19:31 +0000

    12. Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    This script contains a function that displays some
+    stats about nginx logs stored in mongoDb
+"""
+import pymongo
+
+
+if __name__ == '__main__':
+    server = pymongo.MongoClent('mongodb://localhost:27017/')
+    x = server.logs.nginx
+    docs = x.count_documents({})
+    print(f"{docs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+
+    for method in methods:
+        num_of_docs = x.count_documents({"method": methods})
+        print(f"\t{method}: {num_of_docs}")
+
+    status_count = x.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
