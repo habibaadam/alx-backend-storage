84b51c4c039dd8b9862bea4fff42527c47dbd670
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-27 13:57:55 +0000

    0. Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 """
-Module contains a script that stores strings into the
-redis server
+create a Cache class that will implement a simple cache using Redis
 """
+
 import redis
 import uuid
 from typing import Union, Callable, Optional
 
 
 class Cache:
-    """ Stores data in a data key """
+    """
+    a class that stores redis instance as a private variable
+    """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """
-        Initializes a class that enables writing of a string
-        into a redis server or client
+        constructor for Cache class
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
-        """Method that generates a key randomly to store data"""
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        store the input data in redis using a random key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -29,9 +32,9 @@ class Cache:
             fn: Optional[callable] = None) -> Union[str, bytes,
                                                     int, float]:
         """
-         method that take a key string argument and an
-         optional Callable argument named fn. This callable will be used
-         to convert the data back to the desired format
+        method that take a key string argument and an
+        optional Callable argument named fn. This callable will be used
+        to convert the data back to the desired format
         """
         value = self._redis.get(key)
         if value is None:
