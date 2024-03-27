16a9c06150817a9a261f44c1d04f718fe3989500
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-27 14:23:34 +0000

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,6 +21,18 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """stores record of method calls"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper for recording calls"""
+        self._redis.rpush(method.__qualname__ + ":inputs", str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(method.__qualname__ + ":outputs", str(result))
+        return result
+    return wrapper
+
+
 class Cache:
     """
     a class that stores redis instance as a private variable
@@ -34,6 +46,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store the input data in redis using a random key
